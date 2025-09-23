47fcf9cbfb3d684ca00746e16a8fc013d453facb
HenryTene <henry_tene@hotmail.com>
2025-09-23 11:17:04 -0500

    :sparkles: agregar validación al formulario de postulación

--- app/Http/Livewire/PostularVacante.php
+++ app/Http/Livewire/PostularVacante.php
@@ -6,6 +6,21 @@ use Livewire\Component;
 
 class PostularVacante extends Component
 {
+    public $cv;
+    protected $rules = [
+        'cv' => 'required|mimes:pdf'
+    ];
+    public function postularme()
+    {
+        $this->validate();
+        // Almacenar el cv en el disco duro
+
+        // Crear la vacante
+
+        // Crear la notificacion y enviar el email
+
+        // Mostrar mensaje
+    }
     public function render()
     {
         return view('livewire.postular-vacante');
--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,11 +1,14 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col items-center justify-center">
     <h3 class="text-center text-2l font-bold my-4">Postularme a esta vacante</h3>
 
-    <form class="w-96 mt-5 ">
+    <form wire:submit.prevent='postularme' class="w-96 mt-5 ">
         <div class="mb-4">
             <x-label for="cv" :value="__('Curriculum o Hoja de vida(PDF)')"/>
-            <x-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full"/>
+            <x-input id="cv" type="file" wire:model="cv" accept=".pdf" class="block mt-1 w-full"/>
         </div>
+        @error('cv')
+            <livewire:mostrar-alerta :message="$message"/>
+        @enderror
 
         <x-primary-button class="my-5">
             {{ __('Postularme') }}
