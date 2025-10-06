4c6b6e40a7cfd72f655ddbfd639082d5358a97f8
HenryTene <henry_tene@hotmail.com>
2025-10-06 10:17:01 -0500

    :sparkles: crear blade template para listado de candidatos

--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -10,7 +10,7 @@
                 <p class="text-gray-500">Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
             </div>
             <div class="flex flex-col md:flex-row items-stretch gap-3  mt-5 md:mt-0">
-                <a href="#"
+                <a href="{{ route('candidatos.index', $vacante->id) }}"
                     class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold  uppercase text-center">Candidatos</a>
                 <a href="{{ route('vacantes.edit', $vacante->id) }}"
                     class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold  uppercase text-center">Editar</a>
