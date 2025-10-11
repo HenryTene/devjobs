6d1fa3a0053b23a83d139e92729d02cc0c07b190
HenryTene <henry_tene@hotmail.com>
2025-10-11 11:04:47 -0500

    :sparkles: mostrar todas las vacantes disponibles

--- resources/views/livewire/home-vacantes.blade.php
+++ resources/views/livewire/home-vacantes.blade.php
@@ -1,3 +1,38 @@
 <div>
-    <p>Vacantes....</p>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h3 class="font-extrabold text-4xl text-gray-700 mb-12">Nuestras vacantes disponibles</h3>
+            <div class="bg-white shadow-sm rounded-lg p-6 divide-y divide-gray-200">
+                @forelse ($vacantes as $vacante)
+                <div class="md:flex md:justify-between md:items-center py-5">
+                    <div class="md:flex-1">
+                        <a class="text-3xl font-extrabold tex-gray-600 " href="{{route('vacantes.show',$vacante->id)}}">
+                            {{ $vacante->titulo }}
+                        </a>
+                        <p class="text-base text-gray-600 mb-1">
+                            {{ $vacante->empresa }}
+                        </p>
+                        <p class="font-bold text-xs text-gray-600">
+                            Último día para postularse:
+                            <span class="font-normal">{{ $vacante->ultimo_dia->toFormattedDateString() }}</span>
+
+                        </p>
+
+                    </div>
+                    <div class="mt-5 md:mt-0">
+                        <a class="bg-indigo-500 hover:bg-indigo-600 p-3 text-sm uppercase font-bold text-white rounded-lg block text-center"
+                            href="{{route('vacantes.show',$vacante->id)}}">Ver vacante</a>
+                    </div>
+
+                </div>
+
+                @empty
+                <p class="text-center text-gray-600 text-sm">No hay vacantes disponibles</p>
+                @endforelse
+            </div>
+
+
+        </div>
+
+    </div>
 </div>
